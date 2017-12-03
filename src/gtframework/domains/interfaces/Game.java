e5e4e4f67a650dbbb8824c03f3ef7f50a2c66706
Young D3v <h4ck1n6.d3v@gmail.com>
2017-12-04 00:39:01 +0100

    Create skeleton for the domain-related interfaces.

---
+++ src/gtframework/domains/interfaces/Game.java
@@ -0,0 +1,8 @@
+package gtframework.domains.interfaces;
+
+/**
+ * Created by YoungD3v on 4.12.2017.
+ */
+public interface Game {
+
+}
---
+++ src/gtframework/domains/interfaces/GameState.java
@@ -0,0 +1,7 @@
+package gtframework.domains.interfaces;
+
+/**
+ * Created by YoungD3v on 4.12.2017.
+ */
+public interface GameState {
+}
